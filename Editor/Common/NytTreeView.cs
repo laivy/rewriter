a2053b086c0e2e56d8bb1bae2c114dec9904bfbb
LaivY <laivy0629@naver.com>
2023-01-24 21:29:53 +0900

    바이너리 파일 저장, 불러오기
    - 릴리즈 모드에서는 바이너리로 읽고 씀
    - 파일 불러왔을 때 노드 이름이 제대로 표시안되던 것 수정

--- Editor/Common/NytTreeView.cs
+++ Editor/Common/NytTreeView.cs
@@ -40,7 +40,18 @@ namespace Editor.Nyt
 			}
 			streamWriter.Close();
 #else
+			FileStream fileStream = new FileStream(filePath, FileMode.OpenOrCreate);
+			BinaryWriter binaryWriter = new BinaryWriter(fileStream);
+			binaryWriter.Write(Nodes.Count);
 
+			IEnumerator iter = Nodes.GetEnumerator();
+			while (iter.MoveNext())
+			{
+				NytTreeNode node = (NytTreeNode)iter.Current;
+				node.Save(binaryWriter);
+			}
+			binaryWriter.Close();
+			fileStream.Close();
 #endif
 		}
 
@@ -49,14 +60,23 @@ namespace Editor.Nyt
 #if DEBUG
 			StreamReader streamReader = new StreamReader(filePath);
 			int nodeCount = int.Parse(streamReader.ReadLine());
-			for (int i = 0; i < nodeCount; i++)
+			for (int i = 0; i < nodeCount; ++i)
 			{
 				Add(new NytTreeNode(streamReader));
 				SelectedNode = null; // 이렇게 해야 최상위 노드로 추가됨
 			}
 			streamReader.Close();
 #else
-
+			FileStream fileStream = new FileStream(filePath, FileMode.Open);
+			BinaryReader binaryReader = new BinaryReader(fileStream);
+			int nodeCount = binaryReader.ReadInt32();
+			for (int i = 0; i < nodeCount; ++i)
+			{
+				Add(new NytTreeNode(binaryReader));
+				SelectedNode = null;
+			}
+			binaryReader.Close();
+			fileStream.Close();
 #endif
 		}
 	}
