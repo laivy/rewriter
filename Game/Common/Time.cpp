183974ef9a9cc6c94b6f30388efa5120e765b289
LaivY <laivy0629@naver.com>
2023-10-20 00:12:20 +0900

    [기타] 시간 관련 클래스 추가
    - std::chrono 기반
    - 날짜, 시간 관련은 전부 이 클래스를 쓰도록 할 것임

---
+++ Game/Common/Time.cpp
@@ -0,0 +1,103 @@
+﻿#include "Time.h"
+
+using namespace std::chrono;
+using namespace std::chrono_literals;
+
+Time::Time() : m_time{}
+{
+}
+
+Time Time::Now()
+{
+	return Time{ system_clock::to_time_t(system_clock::now()) };
+}
+
+bool Time::operator<(const Time& rhs)
+{
+	return m_time < rhs.m_time;
+}
+
+bool Time::operator<=(const Time& rhs)
+{
+	return m_time <= rhs.m_time;
+}
+
+bool Time::operator>(const Time& rhs)
+{
+	return m_time > rhs.m_time;
+}
+
+bool Time::operator>=(const Time& rhs)
+{
+	return m_time >= rhs.m_time;
+}
+
+bool Time::operator==(const Time& rhs)
+{
+	return m_time == rhs.m_time;
+}
+
+int Time::Year() const
+{
+	return static_cast<int>(GetYMD().year());
+}
+
+unsigned int Time::Month() const
+{
+	return static_cast<unsigned int>(GetYMD().month());
+}
+
+unsigned int Time::Day() const
+{
+	return static_cast<unsigned int>(GetYMD().day());
+}
+
+int Time::Hour() const
+{
+	return GetHMS().hours().count();
+}
+
+int Time::Min() const
+{
+	return GetHMS().minutes().count();
+}
+
+int Time::Sec() const
+{
+	return static_cast<int>(GetHMS().seconds().count());
+}
+
+int Time::Milli() const
+{
+	return static_cast<int>(GetHMS().subseconds().count());
+}
+
+std::tuple<int, int, int> Time::YMD() const
+{
+	return std::make_tuple(Year(), Month(), Day());
+}
+
+std::tuple<int, int, int> Time::HMS() const
+{
+	return std::make_tuple(Hour(), Min(), Sec());
+}
+
+Time::Time(std::time_t time) : m_time{ time }
+{
+}
+
+local_time<system_clock::duration> Time::GetLocalTime() const
+{
+	return zoned_time{ current_zone(), system_clock::from_time_t(m_time) }.get_local_time();
+}
+
+year_month_day Time::GetYMD() const
+{
+	return year_month_day{ floor<days>(GetLocalTime()) };
+}
+
+hh_mm_ss<milliseconds> Time::GetHMS() const
+{
+	auto localTime{ GetLocalTime() };
+	return hh_mm_ss{ floor<milliseconds>(localTime - floor<days>(localTime)) };
+}
---
+++ Game/Common/Time.h
@@ -0,0 +1,82 @@
+﻿#pragma once
+#include <chrono>
+
+template <class T>
+concept IsChronoDuration = requires
+{
+	std::is_same_v<T, std::chrono::year>;
+	std::is_same_v<T, std::chrono::years>;
+	std::is_same_v<T, std::chrono::month>;
+	std::is_same_v<T, std::chrono::months>;
+	std::is_same_v<T, std::chrono::day>;
+	std::is_same_v<T, std::chrono::days>;
+	std::is_same_v<T, std::chrono::hours>;
+	std::is_same_v<T, std::chrono::minutes>;
+	std::is_same_v<T, std::chrono::seconds>;
+};
+
+class Time
+{
+public:
+	Time();
+
+	static Time Now();
+
+	bool operator<(const Time& rhs);
+	bool operator<=(const Time& rhs);
+	bool operator>(const Time& rhs);
+	bool operator>=(const Time& rhs);
+	bool operator==(const Time& rhs);
+
+	template <class T>
+	requires IsChronoDuration<T>
+	Time operator+(const T& duration)
+	{
+		if constexpr (std::is_same_v<T, std::chrono::year>)
+		{
+			std::chrono::years years{ static_cast<int>(duration) };
+			return *this + years;
+		}
+		else if constexpr (std::is_same_v<T, std::chrono::month>)
+		{
+			std::chrono::months months{ static_cast<unsigned int>(duration) };
+			return *this + months;
+		}
+		else if constexpr (std::is_same_v<T, std::chrono::day>)
+		{
+			std::chrono::days days{ static_cast<unsigned int>(duration) };
+			return *this + days;
+		}
+		else
+		{
+			return Time{ m_time + std::chrono::duration_cast<std::chrono::seconds>(duration).count() };
+		}
+	}
+
+	//template <class T>
+	//requires IsChronoYMD<T> || IsChronoHMS<T>
+	//Time& operator+=(const T& duration)
+	//{
+	//	m_time += std::chrono::duration_cast<std::chrono::seconds>(duration);
+	//	return *this;
+	//}
+
+	int Year() const;
+	unsigned int Month() const;
+	unsigned int Day() const;
+	int Hour() const;
+	int Min() const;
+	int Sec() const;
+	int Milli() const;
+	std::tuple<int, int, int> YMD() const;
+	std::tuple<int, int, int> HMS() const;
+
+private:
+	Time(std::time_t time);
+	std::chrono::local_time<std::chrono::system_clock::duration> GetLocalTime() const;
+	std::chrono::year_month_day GetYMD() const;
+	std::chrono::hh_mm_ss<std::chrono::milliseconds> GetHMS() const;
+
+private:
+	std::time_t m_time;
+};
