9a7225f9a2ce42bb7526ce228d2136211014e2b1
LaivY <laivy0629@naver.com>
2024-03-27 20:35:34 +0900

    [기타] 문자열 유틸 함수 수정
    - 멀티바이트 문자열은 mbs, 와이드 문자열을 ws, UTF8 문자열은 u8s로 표기
    - u8stombs 함수 구현

--- Tool/Engine/Util.cpp
+++ Tool/Engine/Util.cpp
@@ -4,12 +4,21 @@
 
 namespace Util
 {
-	std::string u8tos(const std::u8string& u8str)
+	std::string u8stombs(const std::u8string& u8str)
 	{
-		return std::string{ u8str.begin(), u8str.end() };
+		// u8 -> ws
+		int length{ ::MultiByteToWideChar(CP_UTF8, NULL, reinterpret_cast<const char*>(u8str.data()), static_cast<int>(u8str.size()), NULL, NULL) };
+		std::wstring wstr(length, '\0');
+		::MultiByteToWideChar(CP_UTF8, NULL, reinterpret_cast<const char*>(u8str.data()), static_cast<int>(u8str.size()), wstr.data(), length);
+
+		// ws -> mbs
+		length = ::WideCharToMultiByte(CP_ACP, NULL, wstr.data(), static_cast<int>(wstr.size()), NULL, NULL, NULL, NULL);
+		std::string str(length, '\0');
+		::WideCharToMultiByte(CP_ACP, NULL, wstr.data(), static_cast<int>(wstr.size()), str.data(), length, NULL, NULL);
+		return str;
 	}
 
-	std::string wstos(const std::wstring& wstr)
+	std::string wstou8s(const std::wstring& wstr)
 	{
 		int length{ ::WideCharToMultiByte(CP_UTF8, NULL, wstr.data(), static_cast<int>(wstr.size()), NULL, NULL, NULL, NULL) };
 		std::string str(length, '\0');
--- Tool/Engine/Util.h
+++ Tool/Engine/Util.h
@@ -2,6 +2,6 @@
 
 namespace Util
 {
-	std::string u8tos(const std::u8string& u8str);
-	std::string wstos(const std::wstring& wstr);
+	std::string u8stombs(const std::u8string& u8str);
+	std::string wstou8s(const std::wstring& wstr);
 }
