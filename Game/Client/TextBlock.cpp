0ce0b9bc899f981ab5934cedb3f685fa915525cb
LaivY <laivy0629@naver.com>
2024-06-04 21:46:27 +0900

    [클라] TextBlock 구현 - 단순히 텍스트를 보여주는 컨트롤

---
+++ Game/Client/TextBlock.cpp
@@ -0,0 +1,21 @@
+﻿#include "stdafx.h"
+#include "Renderer2D.h"
+#include "TextBlock.h"
+
+TextBlock::TextBlock(IWindow* owner) :
+	IControl{ owner }
+{
+	m_textFormat = Renderer2D::CreateTextFormat(L"", 16);
+}
+
+void TextBlock::Render() const
+{
+	if (m_textFormat)
+		Renderer2D::DrawText(m_position, m_textLayout, D2D1::ColorF::Black);
+}
+
+void TextBlock::SetText(std::wstring_view text)
+{
+	m_text = text;
+	m_textLayout = Renderer2D::CreateTextLayout(m_text, m_textFormat, m_size.x, m_size.y);
+}
---
+++ Game/Client/TextBlock.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Control.h"
+
+class TextBlock : public IControl
+{
+public:
+	TextBlock(IWindow* owner);
+	~TextBlock() = default;
+
+	virtual void Render() const override final;
+
+	void SetText(std::wstring_view text);
+
+private:
+	ComPtr<IDWriteTextFormat> m_textFormat;
+	ComPtr<IDWriteTextLayout> m_textLayout;
+	std::wstring m_text;
+};
