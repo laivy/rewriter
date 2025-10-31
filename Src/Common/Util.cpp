d93ace39ce0159a91bb596f6df17d3a8f75091a6
laivy <laivy0629@gmail.com>
2025-10-31 13:09:27 +0900

    [기타] 불필요한 ImGui 유틸 함수 제거
    - /utf-8 옵션을 사용하기 때문에 u8string 사용하지 않아도 됨

--- Src/Common/Util.cpp
+++ Src/Common/Util.cpp
@@ -21,38 +21,3 @@ namespace Util
 		return wstr;
 	}
 }
-
-#ifdef _IMGUI
-namespace ImGui
-{
-	bool Button(const std::string& label, const ImVec2& size)
-	{
-		return Button(label.c_str(), size);
-	}
-
-	bool Button(const std::wstring& label, const ImVec2& size)
-	{
-		return Button(Util::ToU8String(label).c_str(), size);
-	}
-
-	bool Button(const std::u8string& label, const ImVec2& size)
-	{
-		return Button(reinterpret_cast<const char*>(label.c_str()), size);
-	}
-
-	bool Selectable(const std::string& label, bool selected, ImGuiSelectableFlags flags, const ImVec2& size)
-	{
-		return Selectable(label.c_str(), selected, flags, size);
-	}
-
-	bool Selectable(const std::wstring& label, bool selected, ImGuiSelectableFlags flags, const ImVec2& size)
-	{
-		return Selectable(Util::ToU8String(label).c_str(), selected, flags, size);
-	}
-
-	bool Selectable(const std::u8string& label, bool selected, ImGuiSelectableFlags flags, const ImVec2& size)
-	{
-		return Selectable(reinterpret_cast<const char*>(label.c_str()), selected, flags, size);
-	}
-}
-#endif
--- Src/Common/Util.h
+++ Src/Common/Util.h
@@ -5,16 +5,3 @@ namespace Util
 	std::string ToU8String(std::wstring_view wstr);
 	std::wstring ToWString(std::string_view str);
 }
-
-#ifdef _IMGUI
-namespace ImGui
-{
-	// C++ 표준 호환용 함수
-	bool Button(const std::string& label, const ImVec2& size = ImVec2{ 0, 0 });
-	bool Button(const std::wstring& label, const ImVec2& size = ImVec2{ 0, 0 });
-	bool Button(const std::u8string& label, const ImVec2& size = ImVec2{ 0, 0 });
-	bool Selectable(const std::string& label, bool selected = false, ImGuiSelectableFlags flags = ImGuiSelectableFlags_None, const ImVec2& size = ImVec2{ 0, 0 });
-	bool Selectable(const std::wstring& label, bool selected = false, ImGuiSelectableFlags flags = ImGuiSelectableFlags_None, const ImVec2& size = ImVec2{ 0, 0 });
-	bool Selectable(const std::u8string& label, bool selected = false, ImGuiSelectableFlags flags = ImGuiSelectableFlags_None, const ImVec2& size = ImVec2{ 0, 0 });
-}
-#endif
